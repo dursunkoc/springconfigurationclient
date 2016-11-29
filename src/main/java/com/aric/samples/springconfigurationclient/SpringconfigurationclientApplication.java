c1b1ad96aa20bd7bcd7e79dee31fa2240ae3b2c9
dursunkoc <dursunKoc@gmail.com>
2016-11-29 23:16:47 +0300

    initial commit

---
+++ src/main/java/com/aric/samples/springconfigurationclient/SpringconfigurationclientApplication.java
@@ -0,0 +1,14 @@
+package com.aric.samples.springconfigurationclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringconfigurationclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringconfigurationclientApplication.class, args);
+	}
+	
+	
+}
